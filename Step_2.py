328253f400af7721ed5db2c4859a77b83a629e2f
Erniess <dragon_a@rambler.ru>
2019-05-16 11:03:17 +0300

    Create to JSON module

---
+++ Step_2.py
@@ -0,0 +1,36 @@
+#
+# Чтобы передавать данные между функциями, модулями или разными системами
+# используются форматы данных. Одним из самых популярных форматов является JSON.
+#
+# Напишите декоратор to_json, который можно применить к различным функциям,
+# чтобы преобразовывать их возвращаемое значение в JSON-формат.
+# Не забудьте про сохранение корректного имени декорируемой функции.
+import json
+
+def to_json(func):
+    def wrapped(*args, **kwargs):
+        answ = json.dumps(func(*args, **kwargs))
+        print(answ)
+        return answ
+    return wrapped
+
+@to_json
+def get_dict():
+    return {"a": 42} # Возврат словаря
+
+@to_json
+def get_sum(a, b):
+    return a + b # Возврат вычисления
+
+@to_json
+def get_list():
+    return ["a", None, 142, "54\'", '""'] # Возврат кортежа
+
+@to_json
+def get_obj():
+    return list(filter(bool, range(3)))# Возврат объекта
+
+get_dict()
+get_sum(54, 56)
+get_list()
+get_obj()
